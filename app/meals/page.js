85f1fa02ec3e7b065db4e64005c647b3a9ff88a4
lucasgabwinter <lucasgabwinter@gmail.com>
2024-02-12 11:23:52 -0300

    third commit

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,14 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '../components/meals-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-export default async function Meals() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +25,11 @@ export default async function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals....</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );

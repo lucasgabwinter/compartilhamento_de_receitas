a918a8a0fc77d0b02abb0e3c29e19d89e04d3d5f
lucasgabwinter <lucasgabwinter@gmail.com>
2024-02-12 10:58:15 -0300

    second commit

--- app/components/meal-item.js
+++ app/components/meal-item.js
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import Image from 'next/image';
+
 import classes from './meal-item.module.css';
 
 export default function MealItem({ title, slug, image, summary, creator }) {
--- app/components/meals-grid.js
+++ app/components/meals-grid.js
@@ -4,11 +4,11 @@ import classes from './meals-grid.module.css';
 export default function MealsGrid({ meals }) {
   return (
     <ul className={classes.meals}>
-      {meals.map((meal) => {
+      {meals.map((meal) => (
         <li key={meal.id}>
           <MealItem {...meal} />
-        </li>;
-      })}
+        </li>
+      ))}
     </ul>
   );
 }
